427fea5c0385270019b399a2dd5e03f7a0d3a177
Alexis Amadei <alexisamadei@gmail.com>
2023-01-09 18:12:32 +0100

    upload

---
+++ kea_dashboard/src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div className="Header">
+        <header className="Header-header">
+            <p>
+            KEA PARTNERS DASHBOARD
+            </p>
+        </header>
+        </div>
+    );
+}
