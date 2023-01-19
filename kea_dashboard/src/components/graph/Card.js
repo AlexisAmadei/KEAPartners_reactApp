ca2c0166b3479b3b2b42603e9a2c2ab31bd4106d
Alexis Amadei <alexisamadei@gmail.com>
2023-01-19 18:14:53 +0100

    remove useless code

--- kea_dashboard/src/components/graph/Card.js
+++ kea_dashboard/src/components/graph/Card.js
@@ -4,18 +4,6 @@ import './Card.css'
 
 
 export default function CompteursCard(props) {
-    const cardStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        width: '33%',
-        backgroundColor: '#f7f9fb',
-        borderRadius: '8px',
-        border: '1px solid rgba(77, 77, 77, 0.3)',
-        padding: '15px 8px',
-        height: '89px',
-        width: '142px',
-    }
-
     return (
         <div className='card'>
             <div className="card-title">
