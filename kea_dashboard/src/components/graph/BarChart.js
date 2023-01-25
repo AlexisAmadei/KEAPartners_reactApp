1821e2475ce9ec96d91ef00fc06893c9b47a9687
Alexis Amadei <alexisamadei@gmail.com>
2023-01-25 18:03:04 +0100

    udpate bar chart

--- kea_dashboard/src/components/graph/BarChart.js
+++ kea_dashboard/src/components/graph/BarChart.js
@@ -6,22 +6,18 @@ const BarChart = ({ data /* see data tab */ }) => (
     <ResponsiveBar
         data={data}
         keys={[
-            'hot dog',
-            'burger',
-            'sandwich',
-            'kebab',
-            'fries',
-            'donut'
+            'value'
         ]}
-        indexBy="country"
-        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        indexBy="tribu"
+        margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
         minValue={0}
-        maxValue={100}
+        maxValue={90}
         groupMode="grouped"
         valueScale={{ type: 'linear' }}
         indexScale={{ type: 'band', round: true }}
         colors={['#AF0929']}
         colorBy="index"
+        padding={0.7}
         defs={[
             {
                 id: 'dots',
@@ -42,20 +38,6 @@ const BarChart = ({ data /* see data tab */ }) => (
                 spacing: 10
             }
         ]}
-        fill={[
-            {
-                match: {
-                    id: 'fries'
-                },
-                id: 'dots'
-            },
-            {
-                match: {
-                    id: 'sandwich'
-                },
-                id: 'lines'
-            }
-        ]}
         borderRadius={4}
         axisTop={null}
         axisRight={null}
@@ -63,54 +45,21 @@ const BarChart = ({ data /* see data tab */ }) => (
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: 'country',
-            legendPosition: 'middle',
-            legendOffset: 32
+            // legend: 'country',
+            // legendPosition: 'middle',
+            // legendOffset: 32
         }}
         axisLeft={{
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: 'food',
-            legendPosition: 'middle',
-            legendOffset: -40
+            // legend: 'food',
+            // legendPosition: 'middle',
+            // legendOffset: -40
         }}
         enableLabel={false}
         labelSkipWidth={12}
         labelSkipHeight={12}
-        labelTextColor={{
-            from: 'color',
-            modifiers: [
-                [
-                    'darker',
-                    1.6
-                ]
-            ]
-        }}
-        legends={[
-            {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ]}
         role="application"
         ariaLabel="Nivo bar chart demo"
         barAriaLabel={function (e) { return e.id + ": " + e.formattedValue + " in country: " + e.indexValue }}
--- kea_dashboard/src/pages/Objectifs.js
+++ kea_dashboard/src/pages/Objectifs.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Objectifs.css";
 import data from '../data/barchart.json'
 import BarChart from '../components/graph/BarChart';
-
+import ProgressBar from "@ramonak/react-progress-bar";
 import ObjGauge from '../assets/ObjGauge.svg';
 
 export default function Objectifs() {
@@ -13,9 +13,26 @@ export default function Objectifs() {
                 <img src={ObjGauge} alt="objectif-gauge" />
             </div>
             <div className="obj-results">
-                <span>Résultats</span>
-                <div className="obj-barChart">
-                    <BarChart data={data} />
+                <div style={{ marginRight: '16px' }}>
+                    <span>Résultats</span>
+                    <div className="obj-barChart">
+                        <BarChart data={data} />
+                    </div>
+                </div>
+                <div className="container-pgbar">
+                    <span>haha la dataviz</span>
+                    <div className="container-progress-bar">
+                        <h2>Fiches impact réalisées</h2>
+                        <div    >
+                            <ProgressBar
+                                completed={75}
+                                bgColor="#AF0929"
+                                baseBgColor="#E0E0E0"
+                                labelColor="white"
+                                labelAlignment="center"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
