8e9bf1269790b784dc0396c7b217ca4e3a3adf31
Adameboumahdi <adame.boumahdi@gmail.com>
2023-01-25 12:18:55 +0100

    engagement page

--- kea_dashboard/src/pages/Engagements.js
+++ kea_dashboard/src/pages/Engagements.js
@@ -1,9 +1,13 @@
 import React from "react";
+import todolist from "../assets/graph/todolist.svg"
+import ranked from "../assets/graph/ranked.svg"
 
 export default function Engagements() {
     return (
         <div>
-            <h1>Engagements</h1>
+            <h1>Mes Fiches impacts</h1>
+            <img src={ranked}></img>
+            <img src={todolist}></img>
         </div>
     );
 }
